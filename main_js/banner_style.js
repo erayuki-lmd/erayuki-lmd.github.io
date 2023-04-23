a14a3c9a43ef272cd8a7233c54a62277d23c3bb8
erayuki-lmd <erayuki@lmd.ist.hokudai.ac.jp>
2023-04-23 18:07:05 +0900

    bannerの変更

--- main_js/banner_style.js
+++ main_js/banner_style.js
@@ -3,7 +3,7 @@ $(function () {
         $('.banner_fade').each(function () {
         const scroll = $(window).scrollTop();
         const windowHeight = $(window).height();
-        if (scroll > windowHeight*0.5) {
+        if (scroll > windowHeight*0.1) {
             $(this).addClass('view');
         }
         });
@@ -15,7 +15,7 @@ $(function () {
         $('.banner_fade').each(function () {
         const scroll = $(window).scrollTop();
         const windowHeight = $(window).height();
-        if (scroll < windowHeight*0.5) {
+        if (scroll < windowHeight*0.1) {
             $(this).removeClass('view');
         }
         });
